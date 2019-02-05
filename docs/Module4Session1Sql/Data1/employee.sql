9096b028fcea19bb08e0c516f520c180011f0288
prakash ch <poshgadu@gmail.com>
2019-02-05 13:52:10 +0530

    Documents

---
+++ docs/Module4Session1Sql/Data1/employee.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS employee (
+    `employeeID` INT,
+    `employeeName` VARCHAR(19) CHARACTER SET utf8,
+    `employeeSalary` INT,
+    `employeeDesignation` VARCHAR(18) CHARACTER SET utf8,
+    `employeeMail` VARCHAR(22) CHARACTER SET utf8
+);
+INSERT INTO employee VALUES
+    (43123,'Chanda Bhagat',71287,'Associate Manager','wagnerch@sbcglobal.net'),
+    (421314,'Jawahar Chadda',62353,'Assitant Manager','skaufman@att.net'),
+    (124513,'Ramaeshwara Viswan',51235,'Assitant Manager','pthomsen@comcast.net'),
+    (124624,'Bana Sinha',12461,'Clerk','frostman@sbcglobal.net'),
+    (343262,'Viswarupa Dvivedi',25464,'Technician','dwivedi.visw@bmail.com'),
+    (236685,'Zalim Panda',94747,'Assitant Manager','mrdvt@optonline.net'),
+    (685756,'Kusika Ahuja',43267,'Senior Technician','schwaang@msn.com'),
+    (235644,'Vijya Gulati',28563,'Technician','geekoid@msn.com'),
+    (546334,'Chand Poddar',42647,'Senior Technician','mleary@live.com'),
+    (346735,'Ranjan Sarkar',42647,'Assitant Manager','padme@gmail.com'),
+    (236534,'Gupta Zohana ',87324,'Big Data Scientist','ovprit@breakfast.net'),
+    (980787,'Neha Nayar',67544,'Assitant Manager','larry@cisco.net'),
+    (346357,'Parachure Deshmukh ',13426,'Assitant Manager','preneel@cloud9.com'),
+    (893430,'Shruti Aasiya ',94266,'Senior Developer','heidrich@cloudera.com'),
+    (124536,'Bajpeyi Sarojini ',43647,'Developer','ovprit@comcast.net'),
+    (984738,'Kapadia Satya ',39824,'Assitant Manager','larry@att.net'),
+    (347832,'Dvivedi Aaliya ',76435,'Engineer','preneel@icloud.com'),
+    (127895,'Priya Nambiyar',13257,'Assitant Manager','heidrich@gmail.com'),
+    (258970,'Upalekar Jindan',45323,'Manager','geekoid@gmail.com');
